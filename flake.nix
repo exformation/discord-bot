5dc1bff2542b2a29aaa0c2dc9d84dadf9a726e5c
exform <mmillwood256@gmail.com>
2023-07-19 17:04:28 -0500

    boop

--- flake.nix
+++ flake.nix
@@ -2,34 +2,33 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+    in with pkgs;
+    with python311Packages; {
+      devShells.x86_64-linux.default = mkShell {
+        buildInputs = [
           nil
           nixfmt
           python311
-          python311Packages.python-lsp-server
-          python311Packages.autopep8
-          python311Packages.discordpy
-          python311Packages.requests
+          python-lsp-server
+          autopep8
+          discordpy
+          requests
         ];
       };
-      packages.x86_64-linux.default = with pkgs.python311Packages;
-        buildPythonPackage {
-          name = "discord-bot";
-          pname = "discord-bot";
-          version = "0.0.1";
-          src = ./.;
-          propagatedBuildInputs = [ discordpy requests ];
-        };
-      packages.armv7l-linux.default = with pkgs.python311Packages;
-        buildPythonPackage {
-          name = "discord-bot";
-          pname = "discord-bot";
-          version = "0.0.1";
-          src = ./.;
-          propagatedBuildInputs = [ discordpy requests ];
-        };
+      packages.x86_64-linux.default = buildPythonPackage {
+        name = "discord-bot";
+        pname = "discord-bot";
+        version = "0.0.1";
+        src = ./.;
+        propagatedBuildInputs = [ discordpy requests ];
+      };
+      packages.armv7l-linux.default = buildPythonPackage {
+        name = "discord-bot";
+        pname = "discord-bot";
+        version = "0.0.1";
+        src = ./.;
+        propagatedBuildInputs = [ discordpy requests ];
+      };
 
     };
 }
